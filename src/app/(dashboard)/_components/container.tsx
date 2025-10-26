d7643e5992aa08c1a328c78d8a8f8f64c42dda70
yoohyojun <hyojun99222@gmail.com>
2025-10-26 15:40:04 +0900

    feat: 레이아웃 폰트 색상 수정

--- src/app/(dashboard)/_components/container.tsx
+++ src/app/(dashboard)/_components/container.tsx
@@ -10,10 +10,10 @@ export function Container({ title, description, children }: ContainerProps) {
   return (
     <main className="px-[5rem] py-[2.5rem]">
       <header className="mb-[2rem]">
-        <h1 className="mb-[0.5rem] text-[2rem] font-bold text-[#374553]">
+        <h1 className="mb-[0.5rem] text-[2rem] font-bold text-[#28323C]">
           {title}
         </h1>
-        <p className="text-[1rem] text-[#646F7C]">{description}</p>
+        <p className="text-[1rem] text-[#374553]">{description}</p>
       </header>
       {children}
     </main>

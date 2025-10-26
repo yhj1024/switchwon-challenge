620368d3655bf0fe574f2b1e023503c41adf0d7b
yoohyojun <hyojun99222@gmail.com>
2025-10-26 15:42:08 +0900

    feat: 루트 페이지 접근 시 토큰에 따른 분기 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default function RootPage() {
+export default async function RootPage() {
+  const cookieStore = await cookies();
+  const authToken = cookieStore.get("authToken");
+
+  if (authToken) {
+    redirect("/exchange");
+  }
+
   redirect("/login");
 }
